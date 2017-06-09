9ca39809ed69bda5550bc461b430f800b61477db
danielswallace <daniel.shawn.wallace@gmail.com>
2017-06-09 16:22:55 -0600

    initial commit

---
+++ includes/class-wordpress-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://github.com/danielswallace
+ * @since      1.0.0
+ *
+ * @package    Wordpress_Plugin
+ * @subpackage Wordpress_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wordpress_Plugin
+ * @subpackage Wordpress_Plugin/includes
+ * @author     Daniel Wallace <daniel.shawn.wallace@gmail.com>
+ */
+class Wordpress_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wordpress-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://github.com/danielswallace
+ * @since      1.0.0
+ *
+ * @package    Wordpress_Plugin
+ * @subpackage Wordpress_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wordpress_Plugin
+ * @subpackage Wordpress_Plugin/includes
+ * @author     Daniel Wallace <daniel.shawn.wallace@gmail.com>
+ */
+class Wordpress_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
